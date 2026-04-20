2d9dd903348e456e19707a9b17934f74eec73b36
Rafael Clemente <rafaeljc@users.noreply.github.com>
2026-04-20 12:40:40 -0300

    Initial commit

---
+++ src/main/java/io/github/rafaeljc/fintechtransactionapi/FintechTransactionApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.rafaeljc.fintechtransactionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechTransactionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechTransactionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rafaeljc/fintechtransactionapi/FintechTransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rafaeljc.fintechtransactionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechTransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
