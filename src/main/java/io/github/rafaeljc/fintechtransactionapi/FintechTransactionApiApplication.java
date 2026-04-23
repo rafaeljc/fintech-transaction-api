a8afab15ab6dcd55df2b6ceb406a85c7d0b07647
Rafael Clemente <43890345+rafaeljc@users.noreply.github.com>
2026-04-23 12:26:01 -0300

    ci: establish CI pipeline and structured JSON logging foundation (#1)

--- src/main/java/io/github/rafaeljc/fintechtransactionapi/FintechTransactionApiApplication.java
+++ src/main/java/io/github/rafaeljc/fintechtransactionapi/FintechTransactionApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FintechTransactionApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FintechTransactionApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FintechTransactionApiApplication.class, args);
+    }
 
 }
