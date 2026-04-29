35a2a82ed4688b92f5a5f16e225053ed0897ddd3
Rafael Clemente <rafaeljc@users.noreply.github.com>
2026-04-29 09:40:21 -0300

    feat: add debug logging to global exception handlers

--- src/main/java/io/github/rafaeljc/fintechtransactionapi/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/rafaeljc/fintechtransactionapi/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package io.github.rafaeljc.fintechtransactionapi.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void handleNotReadable() {
+    public void handleNotReadable(HttpMessageNotReadableException ex) {
+        LOG.debug("Malformed request body: {}", ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public void handleNotValid() {
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
+    public void handleNotValid(MethodArgumentNotValidException ex) {
+        LOG.debug("Validation failed: {}", ex.getMessage());
     }
 }
